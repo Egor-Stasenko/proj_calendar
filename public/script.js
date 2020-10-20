75f645e2abbbf89aa69005e2183cfc7c0f187fc1
Egor-Stasenko <73098779+Egor-Stasenko@users.noreply.github.com>
2020-10-20 20:24:00 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -5,31 +5,11 @@ onInput_cercleClick = function(num_of_element){
     element.click();
 };
 
-function f() {
-    //alert("1");
-    /*
-    var root = document.getElementById("infoChild");
-    root.innerHTML = '<p class="name underlined underlined--gradient">Название</p>';
-    var d = document.createElement("div");
-    d.innerHTML = "qwer";
-    root.appendChild(d);
-*/
-
-    Array.prototype.forEach.call(document.querySelectorAll(".s1"), function(e){
-        e.style.color = "red";
-
-        //e.classList.toggle("class_name"); //add remove
-
-    });
-    //. - class
-    //# - id
-    //div - element type
-    // -> direct child
-
-
-
-}
-
-document.addEventListener('DOMContentLoaded', function(){
-
-});
+// Array.prototype.forEach.call(document.querySelectorAll(".s1"), function(e){
+//     e.style.color = "red";
+//     //e.classList.toggle("class_name"); //add remove
+//     //. - class
+//     //# - id
+//     //div - element type
+//     // -> direct child
+// });
