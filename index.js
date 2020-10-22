44e51710f58452a143cfd793f9fecf78038ca609
Egor-Stasenko <73098779+Egor-Stasenko@users.noreply.github.com>
2020-10-22 09:10:40 +0300

    1

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var settings = JSON.parse(fs.readFileSync("settings.json"));
 console.log(settings);
 
 app.use(function(req, res, next) {
-  console.log('%s %s', req.method, req.url);
   next();
 })
 
@@ -15,7 +14,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.listen(3000);
-console.log("listening 3000");
+console.log("listening 3000 (http://localhost:3000/)");
 
 var livereload = require("livereload");
 var lrServer = livereload.createServer();
--- public/script.js
+++ public/script.js
@@ -1,9 +1,21 @@
 let inputs_color = document.getElementsByClassName("input_color");
+let inputs_colorCercle = document.getElementsByClassName("input-cercle")
 
-onInput_cercleClick = function(num_of_element){
+inputCircle_onClick = function(num_of_element){
     let element = inputs_color[num_of_element-1];
     element.click();
 };
+
+
+for(let i = 0; i < inputs_color.length; i++){
+    let inp = inputs_color[i];
+    let cer = inputs_colorCercle[i];
+
+    inp.addEventListener("input", function() {
+        cer.style.backgroundColor = inp.value;
+    }, false);
+};
+
 
 // Array.prototype.forEach.call(document.querySelectorAll(".s1"), function(e){
 //     e.style.color = "red";
